260e826de694d9de0d1803a770388aabd197451e
Kader <ian.graphics.213@gmail.com>
2014-07-05 02:32:30 +0100

    Correcting dist css and js files. Adding jQuery scroll property.

--- js/dinakit.js
+++ js/dinakit.js
@@ -4,29 +4,38 @@
  *  License - "Don't Be A Dick License" - DBAD by philsturgeon (http://www.dbad-license.org/)
  */
 
-(function(e){
+(function($){
 
     // close alerts
-    e(document).on("click",".alert-close",function(t){
-        e(this).closest(".alert").slideUp(150,function(){
-            e(this).remove()
+    $(document).on("click",".alert-close",function(t){
+        $(this).closest(".alert").slideUp(150,function(){
+            $(this).remove()
         });
         t.preventDefault()
     });
 
     // close modals
-    e(document).on("click",".modal-close",function(t){
-        e(this).closest(".modal").fadeOut(150,function(){
-            e(this).remove()
+    $(document).on("click",".modal-close",function(t){
+        $(this).closest(".modal").fadeOut(150,function(){
+            $(this).remove()
         });
         t.preventDefault()
     });
 
+    // scroll to
+    $(document).on("click",".scroll",function(t){
+        var target = $(this).attr('href');
+        var pathname = window.location;
+        history.pushState({}, '', $(this).attr("href"));
+        $("html, body").animate({ scrollTop: $(target).offset().top }, 1000);
+        t.preventDefault()
+    });
+
     // automatically add icons to menu items
-    e(".menu > li").has("ul").each(function(){
-        e("> a:not(.no-fa)",this).append('<i class="fa fa-caret-down ml5p op50"></i>');
-        e("ul > li",this).has("ul").each(function(){
-            e("> a:not(.no-fa)",this).append('<i class="fa fa-caret-right"></i>')
+    $(".menu > li").has("ul").each(function(){
+        $("> a:not(.no-fa)",this).append('<i class="fa fa-caret-down ml5p op50"></i>');
+        $("ul > li",this).has("ul").each(function(){
+            $("> a:not(.no-fa)",this).append('<i class="fa fa-caret-right"></i>')
         })
     })
 
